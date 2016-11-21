8efc347b92a2fe2c62cc7dfddd47390158297da7
Michael Boulton <boulton@zoetrope.io>
2016-11-21 09:49:41 +0000

    Add an option to get the auth headers from somewhere else

--- src/paho/mqtt/client.py
+++ src/paho/mqtt/client.py
@@ -453,7 +453,8 @@ class Client(object):
       corresponding unsubscribe() call.
 
     """
-    def __init__(self, client_id="", clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp"):
+    def __init__(self, client_id="", clean_session=True, userdata=None,
+            protocol=MQTTv311, transport="tcp", get_auth_headers=None):
         """client_id is the unique client id string used when connecting to the
         broker. If client_id is zero length or None, then one will be randomly
         generated. In this case, clean_session must be True. If this is not the
@@ -484,6 +485,8 @@ class Client(object):
         """
         if not clean_session and (client_id == "" or client_id is None):
             raise ValueError('A client id must be provided if clean session is False.')
+
+        self._get_auth_headers = get_auth_headers
 
         self._transport = transport
         self._protocol = protocol
@@ -826,9 +829,11 @@ class Client(object):
 
         if self._transport == "websockets":
             if self._tls_ca_certs is not None:
-                self._ssl = WebsocketWrapper(self._ssl, self._host, self._port, True)
+                self._ssl = WebsocketWrapper(self._ssl, self._host, self._port,
+                    True, self._get_auth_headers)
             else:
-                sock = WebsocketWrapper(sock, self._host, self._port, False)
+                sock = WebsocketWrapper(sock, self._host, self._port, False,
+                    self._get_auth_headers)
 
         self._sock = sock
         if self._ssl:
@@ -2615,7 +2620,7 @@ class WebsocketWrapper:
     OPCODE_PING = 0x9
     OPCODE_PONG = 0xa
 
-    def __init__(self, socket, host, port, is_ssl):
+    def __init__(self, socket, host, port, is_ssl, get_auth_headers):
 
         self.connected = False
 
@@ -2631,26 +2636,32 @@ class WebsocketWrapper:
         self._payload_head = 0
         self._readbuffer_head = 0
 
-        self._do_handshake()
+        self._do_handshake(get_auth_headers)
 
     def __del__(self):
 
         self._sendbuffer = None
         self._readbuffer = None
 
-    def _do_handshake(self):
+    def _do_handshake(self, get_auth_headers):
 
         sec_websocket_key = uuid.uuid4().bytes
         sec_websocket_key = base64.b64encode(sec_websocket_key)
 
-        header = b"GET /mqtt HTTP/1.1\r\n" +\
-                 b"Upgrade: websocket\r\n" +\
-                 b"Connection: Upgrade\r\n" +\
-                 b"Host: " + str(self._host).encode('utf-8') + b":" + str(self._port).encode('utf-8') + b"\r\n" +\
-                 b"Origin: http://" + str(self._host).encode('utf-8') + b":" + str(self._port).encode('utf-8') + b"\r\n" +\
-                 b"Sec-WebSocket-Key: " + sec_websocket_key + b"\r\n" +\
-                 b"Sec-WebSocket-Version: 13\r\n" +\
-                 b"Sec-WebSocket-Protocol: mqtt\r\n\r\n"
+        if get_auth_headers:
+            header = get_auth_headers()
+        else:
+            header = "GET /mqtt HTTP/1.1\r\n" +\
+                     "Upgrade: websocket\r\n" +\
+                     "Connection: Upgrade\r\n" +\
+                     "Host: {self._host:s}:{self._port:s}\r\n" +\
+                     "Origin: http://{self._host:s}:{self._port:s}\r\n" +\
+                     "Sec-WebSocket-Key: {sec_websocket_key:s}\r\n" +\
+                     "Sec-WebSocket-Version: 13\r\n" +\
+                     "Sec-WebSocket-Protocol: mqtt\r\n\r\n".format(self=self,
+                         sec_websocket_key=sec_websocket_key)
+
+        header = header.encode("utf8")
 
         if self._ssl:
             self._socket.write(header)
